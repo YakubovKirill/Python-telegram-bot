73883571dce62dc50aca7f9902ee4289e63d885e
Kirill <yakubov.kirill.work@gmail.com>
2020-08-26 10:17:36 +0300

    trach messages

--- bot_main.py
+++ bot_main.py
@@ -1,4 +1,5 @@
 import requests
+from time import sleep
 
 url = "https://api.telegram.org/bot1389526379:AAGNGJc05zu3mH0EBfY8qOmj00jZp5nVYCY/"
 
@@ -19,6 +20,20 @@ def send_mess(chat, text):
     params = {'chat_id': chat, 'text': text}
     response = requests.post(url + 'sendMessage', data=params)
     return response
+def main():  
+    update_id = last_update(get_updates_json(url))['update_id']
+    while True:
+        if update_id == last_update(get_updates_json(url))['update_id']:
+           send_mess(get_chat_id(last_update(get_updates_json(url))), 'test')
+           update_id += 1
+        sleep(1)
+     
+def get_updates_json(request):  
+    params = {'timeout': 100}
+    response = requests.get(request + 'getUpdates', data=params)
+    return response.json()
 
+if __name__ == '__main__':  
+    main()
 chat_id = get_chat_id(last_update(get_updates_json(url)))
-send_mess(chat_id, 'Your message goes here')
+send_mess(chat_id, "Hi from first bot")
